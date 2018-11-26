55204c1eab68d2b81e2aa95ddd6b7c5b63903f48
Allen Melikian <allenwem@gmail.com>
2018-11-25 20:17:41 -0800

    extra loop removed

--- src/reader.c
+++ src/reader.c
@@ -6,7 +6,7 @@
 /*   By: amelikia <amelikia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 17:20:53 by amelikia          #+#    #+#             */
-/*   Updated: 2018/11/25 17:22:30 by amelikia         ###   ########.fr       */
+/*   Updated: 2018/11/25 20:17:29 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@ void	readlines(t_filler *filler)
 	char		*line;
 	int			j;
 
-	while (1)
+	j = 0;
+	while (get_next_line(0, &line) != -1)
 	{
-		j = 0;
-		while (get_next_line(0, &line) != -1)
+		plateau(filler, line);
+		sizeofpiece(filler, line);
+		if (line[0] != 'P' && ft_strncmp(line, "    ", 4) != 0\
+			&& line[0] != '0')
 		{
-			plateau(filler, line);
-			sizeofpiece(filler, line);
-			if (line[0] != 'P' && ft_strncmp(line, "    ", 4) != 0\
-				&& line[0] != '0')
-			{
-				piece(filler, line);
-				free(line);
-				if (j == (filler->piece->height - 1))
-					break ;
-				j++;
-			}
+			piece(filler, line);
+			free(line);
+			if (j == (filler->piece->height - 1))
+				break ;
+			j++;
 		}
-		b_printf("LOL\n");
 	}
+	b_printf("LOL\n");
 }
